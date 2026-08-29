c1ca13c64a9bce3f62170b8c0f9b838aa92bfa83
dopamane <dwc1295@gmail.com>
2026-08-28 20:09:39 -0700

    op status

--- lib/Data/ODID.hs
+++ lib/Data/ODID.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Open Drone ID
-module Data.ODID (ODID(..), readODID, writeODID) where
+module Data.ODID
+  ( ODID(..), readODID, writeODID
+  , UA(..)
+  ) where
 
 import Data.ByteString.Lazy (ByteString)
+import Prettyprinter
 
 data ODID = ODID
 
@@ -10,3 +16,40 @@ readODID = undefined
 
 writeODID :: ODID -> ByteString
 writeODID = undefined
+
+-- | Unmanned aircraft
+data UA
+  = None | Aeroplane | Heli | Gyro | Hybrid | Ornith | Glider | Kite | FreeBalloon
+  | CaptiveBalloon | Airship | Parachute | Rocket | TetheredPwrAircraft | GroundObstacle
+  | Other
+  deriving (Eq, Read, Show)
+
+instance Pretty UA where
+  pretty ua = case ua of
+    None -> "None"
+    Aeroplane -> "Aeroplane"
+    Heli -> "Helicopter"
+    Gyro -> "Gyroplane"
+    Hybrid -> "Hybrid Lift"
+    Ornith -> "Ornithopter"
+    Glider -> "Glider"
+    Kite -> "Kite"
+    FreeBalloon -> "Free Balloon"
+    CaptiveBalloon -> "Captive Balloon"
+    Airship -> "Airship"
+    Parachute -> "Parachute"
+    Rocket -> "Rocket"
+    TetheredPwrAircraft -> "Tethered Powered Aircraft"
+    GroundObstacle -> "Ground Obstacle"
+    Other -> "Other"
+
+-- | Operational status
+data OpStatus
+  = Undeclared | Ground | Airborne | Emergency | RemoteIDSystemFailure | OpStatusRsvd
+  deriving (Eq, Read, Show)
+
+instance Pretty OpStatus where
+  pretty s = case s of
+    RemoteIDSystemFailure -> "Remote ID System Failure"
+    OpStatusRsvd -> "Reserved"
+    _ -> viaShow s
