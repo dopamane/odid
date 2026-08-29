0c83c31924fa10f61b4ab69b7c9769f538408956
dopamane <dwc1295@gmail.com>
2026-08-29 11:49:24 -0700

    read ua type, update basic id decoder

--- lib/Data/ODID.hs
+++ lib/Data/ODID.hs
@@ -3,12 +3,13 @@
 -- | Open Drone ID
 module Data.ODID
   ( ODID(..), readODID, writeODID
-  , UA(..)
+  , UAType(..), readUAType
   ) where
 
 import Data.Binary.Get
 import Data.Bits
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
 import Data.Word
 import Numeric
 import Prettyprinter
@@ -22,13 +23,13 @@ writeODID :: ODID -> ByteString
 writeODID = undefined
 
 -- | Unmanned aircraft
-data UA
+data UAType
   = None | Aeroplane | Heli | Gyro | Hybrid | Ornith | Glider | Kite | FreeBalloon
   | CaptiveBalloon | Airship | Parachute | Rocket | TetheredPwrAircraft | GroundObstacle
   | Other
-  deriving (Eq, Read, Show)
+  deriving (Eq, Enum, Read, Show)
 
-instance Pretty UA where
+instance Pretty UAType where
   pretty ua = case ua of
     None -> "None"
     Aeroplane -> "Aeroplane"
@@ -47,6 +48,11 @@ instance Pretty UA where
     GroundObstacle -> "Ground Obstacle"
     Other -> "Other"
 
+readUAType :: (Eq a, Integral a, Num a, Show a) => a -> Either String UAType
+readUAType t
+  | t >= 0 && t < 16 = Right $ toEnum $ fromIntegral t
+  | otherwise = Left $ "failed to read UAType " ++ show t
+
 -- | Operational status
 data OpStatus
   = Undeclared | Ground | Airborne | Emergency | RemoteIDSystemFailure | OpStatusRsvd
@@ -58,18 +64,20 @@ instance Pretty OpStatus where
     OpStatusRsvd -> "Reserved"
     _ -> viaShow s
 
-data MsgType = BasicID | Location | Auth | SelfID | System | OperatorID | Pack
+data MsgType = BasicIDTy | Location | Auth | SelfIDTy | System | OperatorID | Pack
   deriving (Eq, Read, Show)
 
 instance Pretty MsgType where
-  pretty = viaShow
+  pretty BasicIDTy = "BasicID"
+  pretty SelfIDTy = "SelfID"
+  pretty t = viaShow t
 
 readMsgType :: Word8 -> Either String MsgType
 readMsgType n = case n of
-  0x0 -> Right BasicID
+  0x0 -> Right BasicIDTy
   0x1 -> Right Location
   0x2 -> Right Auth
-  0x3 -> Right SelfID
+  0x3 -> Right SelfIDTy
   0x4 -> Right System
   0x5 -> Right OperatorID
   0xF -> Right Pack
@@ -84,8 +92,39 @@ instance Pretty MsgHdr where
 mkMsgHdr :: Word8 -> Either String MsgHdr
 mkMsgHdr w8 = MsgHdr <$> readMsgType (w8 `shiftR` 4) <*> pure (fromIntegral $ w8 .&. 0xF)
 
-data BasicIDMsgRaw = BasicIDMsgRaw Word8 ByteString
+data MsgBdy
+  = BasicID BasicIDMsg
+  | SelfID
   deriving (Eq, Read, Show)
 
-readBasicIDMsgRaw :: Get BasicIDMsgRaw
-readBasicIDMsgRaw = liftA2 BasicIDMsgRaw getWord8 (getLazyByteString 20) <* getByteString 3
+data BasicIDMsg = BasicIDMsg
+  { basicIDType :: IDType
+  , basicIDUA :: UAType
+  , basicIDUASID :: UASID
+  }
+  deriving (Eq, Read, Show)
+
+getBasicIDMsg :: Get BasicIDMsg
+getBasicIDMsg = do
+  w8     <- getWord8
+  idType <- either fail return $ readIDType $ w8 `shiftR` 4
+  uatype <- either fail return $ readUAType $ w8 .&. 0xF
+  uasID  <- BS.takeWhile (/= 0x00) <$> getLazyByteString 20 <* getByteString 3
+  return $ BasicIDMsg idType uatype uasID
+
+data Msg = Msg{ msgHdr :: MsgHdr, msgBdy :: MsgBdy }
+  deriving (Eq, Read, Show)
+
+data IDType = IDTypeNone | SerialNum | CAARegID | UTMUUID | SpecificSessionID
+  deriving (Eq, Read, Show)
+
+readIDType :: (Eq a, Num a, Show a) => a -> Either String IDType
+readIDType n = case n of
+  0 -> Right IDTypeNone
+  1 -> Right SerialNum
+  2 -> Right CAARegID
+  3 -> Right UTMUUID
+  4 -> Right SpecificSessionID
+  _ -> Left $ "faild to read ID type " ++ show n
+
+type UASID = ByteString
